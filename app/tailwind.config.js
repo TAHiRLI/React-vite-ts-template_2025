4ab6d6947b7e16ce9883378b75cfbd1f740f0065
Shahmar Kazimov <shahmar.kazimoff@gmail.com>
2025-04-01 20:15:22 +0400

    feat:tailwind added. Closes #2

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Yeni eklenen satır
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
